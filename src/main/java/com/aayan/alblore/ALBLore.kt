a57828d5103be93a8b5fc1583d6184bcb35b9a03
aayan khan <133193801+aayankhaan@users.noreply.github.com>
2026-07-03 13:16:35 +0500

    Base setup

---
+++ src/main/java/com/aayan/alblore/ALBLore.kt
@@ -0,0 +1,13 @@
+package com.aayan.alblore
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class ALBLore : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
